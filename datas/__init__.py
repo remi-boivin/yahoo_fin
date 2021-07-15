4ad91889f3844b781e50a5c3b704b39bac8de582
rémi <remi.boivin@scalnyx.com>
2021-07-15 16:10:44 +0200

    remove comments

--- datas/__init__.py
+++ datas/__init__.py
@@ -7,19 +7,11 @@ class Data():
     def __init__(self):
         """ """
 
-<<<<<<< HEAD
-        self.tickers_list = ['msft', 'nflx', 'amzn']
-        # self.tickers_list = stock_info.tickers_sp500()
-=======
-        self.tickers_list = ["nflx"]#stock_info.tickers_sp500()
->>>>>>> add review
+        self.tickers_list = stock_info.tickers_sp500()
         self.tickers_list_data = pd.DataFrame()
 
-    def get_historical_datas(self, date='07/07/2021'):
+    def get_historical_datas(self, date='01/01/1990'):
         """ """
-<<<<<<< HEAD
-        i = 0
-=======
 
         for data in self.tickers_list:
             self.tickers_list_data = pd.concat(
@@ -28,23 +20,11 @@ class Data():
         if 0 in nan_count:
             print(f'You dropped\n {nan_count}\nNaN occurences')
             self.tickers_list_data.dropna()
->>>>>>> add review
 
-        for data in self.tickers_list:
-            self.tickers_list_data = pd.concat([self.tickers_list_data, stock_info.get_data(data, date, index_as_date=True)])
-        if self.tickers_list_data.isnull().values.any() == True:
-            print(f"You have deleted {i} values");
-            i += 1
-        self.tickers_list_data.dropna()
     
     def get_company(self, company):
         """" """
-<<<<<<< HEAD
-        
-        new_df = self.tickers_list_data[(self.tickers_list_data.ticker == company.upper())]
-=======
 
         new_df = self.tickers_list_data[(
-            self.tickers_list_data.tickner == company.upper())]
->>>>>>> add review
+            self.tickers_list_data.ticker == company.upper())]
         return new_df
