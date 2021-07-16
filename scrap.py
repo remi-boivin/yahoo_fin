2d91f779fe49f7cef0e71dc06641f0a2c5f64000
rémi <remi.boivin@scalnyx.com>
2021-07-16 15:29:27 +0200

    split data generation and report generation

--- scrap.py
+++ scrap.py
@@ -4,7 +4,5 @@ from os import environ
 
 if __name__ == "__main__":
     data = Data()
-    data.get_historical_datas('01/01/2020')
+    data.get_historical_datas()
     data.tickers_list_data.to_csv(environ['CSV'])
-    reports = Report()  
-    reports.generate_report(data)
