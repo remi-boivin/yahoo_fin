278acb8d0ed37d86887a057b7868c1189bc77b5d
Rémi <remi.boivin@epitech.eu>
2021-08-09 17:34:27 +0200

    delete description

--- reports/__init__.py
+++ reports/__init__.py
@@ -14,5 +14,4 @@ class Report():
         f = open("datas/report.txt", "w")
         f.write(f"Start date: {data['date'][[data.index[0]][0]]}\nEnds at: {data['date'][[data.index[-1]][0]]}\n")
         f.write(f"{environ['CSV']} as : {len(data.index)} lines\n\n")
-        f.write(f"Exemple {environ['CSV']} contain: \n{data.describe()}\n")
         f.close()
