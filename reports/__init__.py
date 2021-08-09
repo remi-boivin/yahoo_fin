4d46170ba68ca89eb4ecab0a61881529662e17b0
Rémi <remi.boivin@epitech.eu>
2021-08-09 18:04:07 +0200

    add default value to getenv

--- reports/__init__.py
+++ reports/__init__.py
@@ -1,5 +1,5 @@
 from datetime import date
-from os import environ
+from os import environ, getenv
 from yahoo_fin import stock_info
 
 class Report():
@@ -11,7 +11,7 @@ class Report():
 
     def generate_report(self, data):
         """ """
-        f = open("datas/report.txt", "w")
+        f = open(getenv('REPORT', 'datas/report.txt'), 'w+')
         f.write(f"Start date: {data['date'][[data.index[0]][0]]}\nEnds at: {data['date'][[data.index[-1]][0]]}\n")
-        f.write(f"{environ['CSV']} as : {len(data.index)} lines\n\n")
+        f.write(f"{getenv('CSV', 'datas/data.csv')} as : {len(data.index)} lines\n\n")
         f.close()
