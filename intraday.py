90b33e09f084e9f9ca3e6000e8591203fd7a43d4
rémi <remi.boivin@scalnyx.com>
2021-08-17 16:36:51 +0200

    multireading app

--- intraday.py
+++ intraday.py
@@ -1,4 +1,3 @@
-import re
 from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.support.ui import WebDriverWait
@@ -7,9 +6,10 @@ from selenium.webdriver.support import expected_conditions as EC
 from time import sleep
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.common.by import By
-import threading
 from selenium.common.exceptions import NoSuchElementException, TimeoutException, StaleElementReferenceException
 import pandas as pd
+from os import mkdir, path
+from threading import Thread, Lock
 from os import mkdir, path
 import datetime
 
@@ -27,55 +27,53 @@ def get_tickers():
         i += 1
     return tickers_list
 
-def get_cac40(tickers):
-    wait = WebDriverWait(driver, 10)
+def get_cac40(ticker, driver):
+    mutex.acquire()
     try:
-        for ticker in tickers:
-            print("sleep boucle end")
+        elem = WebDriverWait(driver, 5).until(EC.visibility_of_element_located((By.XPATH, '//*[@id="yfin-usr-qry"]')))
+        elem.send_keys(ticker)
+        elem.send_keys(Keys.RETURN)
+        # we need to wait to prevant wrong data
+        sleep(3)
+        try:
+            elem = WebDriverWait(driver, 20).until(EC.visibility_of_element_located((By.XPATH, '//*[@id="quote-header-info"]/div[3]/div[1]/div/span[1]')))
             try:
-                elem = wait.until(EC.visibility_of_element_located((By.XPATH, '//*[@id="yfin-usr-qry"]')))
-                elem.send_keys(ticker)
-                elem.send_keys(Keys.RETURN)
-                try:
-                    elem = wait.until(EC.visibility_of_element_located((By.XPATH, '//*[@id="quote-header-info"]/div[3]/div[1]/div/span[1]')))
-                    try:
-                        df = pd.concat(
-                            [pd.DataFrame(data=elem.get_attribute('innerHTML'),index=[datetime.datetime.now()], columns=['value']), pd.read_csv(f"datas/{ticker}/{ticker}.csv", index_col=0)])
-                        print(f"ticker = {ticker} Element = {df}")
-                        df.to_csv(f"datas/{ticker}/{ticker}.csv")
-                    except FileNotFoundError:
-                        if not path.exists(f'datas/{ticker}'):
-                            mkdir(f"datas/{ticker}")
-                        df = pd.DataFrame(data=elem.get_attribute('innerHTML'),index=[datetime.datetime.now()], columns=['value'])
-                        print(f"ticker = {ticker} Element = {df}")
-                        df.to_csv(f"datas/{ticker}/{ticker}.csv")
-                except NoSuchElementException:
-                    t = threading.Thread(target=driver.get, args=('https://fr.finance.yahoo.com/lookup',))
-                    t.start()
-            except TimeoutException:
-                print(f'No data for {ticker}')
-                t = threading.Thread(target=driver.get, args=('https://fr.finance.yahoo.com/lookup',))
-                t.start()
-        
-            t = threading.Thread(target=driver.get, args=('https://fr.finance.yahoo.com/lookup',))
-            t.start()
+                data_value = elem.get_attribute('innerHTML')
+                df = pd.concat(
+                    [pd.DataFrame(data=data_value,index=[datetime.datetime.now()], columns=['value']), pd.read_csv(f"datas/{ticker}/{ticker}.csv", index_col=0)])
+                print(f"ticker = {ticker} Element = {df}")
+                df.to_csv(f"datas/{ticker}/{ticker}.csv")
+            except FileNotFoundError:
+                if not path.exists(f'datas/{ticker}'):
+                    mkdir(f"datas/{ticker}")
+                df = pd.DataFrame(data=data_value,index=[datetime.datetime.now()], columns=['value'])
+                print(f"ticker = {ticker} Element = {df}")
+                df.to_csv(f"datas/{ticker}/{ticker}.csv")
+        except NoSuchElementException:
+            print('OK')
     except TimeoutException:
-        print('no form')
+        t = Thread(target=driver.get, args=('https://fr.finance.yahoo.com/lookup',))
+        t.start()
+    finally:
+        mutex.release()
 
+mutex = Lock()
+
+def launch_cac40(tickers):
+    wait = WebDriverWait(driver, 10)
+    for ticker in tickers:
+        t1 = Thread(target=get_cac40, args=(ticker, driver))
+        t1.start()
+    t1.join()
 options = Options()
 options.add_argument("start-maximized")
 options.add_argument("disable-infobars")
 options.add_argument("--disable-extensions")
-driver = webdriver.Firefox()
-
-t = threading.Thread(target=driver.get, args=('https://fr.finance.yahoo.com/lookup',))
+driver = webdriver.Chrome()
+t = Thread(target=driver.get, args=('https://fr.finance.yahoo.com/lookup',))
 t.start()
+elem = WebDriverWait(driver, 20).until(EC.element_to_be_clickable((By.XPATH, "/html/body/div/div/div/div/form/div[2]/div[2]/button"))).click()
 tickers = get_tickers()
-# Return to home page to prevent wrong datas
-t = threading.Thread(target=driver.get, args=('https://fr.finance.yahoo.com/lookup',))
-t.start()
-
-WebDriverWait(driver, 20).until(EC.element_to_be_clickable((By.CSS_SELECTOR, "button.btn:nth-child(5)"))).click()
 while (True):
-    get_cac40(tickers)
-    sleep(10)
+    launch_cac40(tickers)
+    sleep(60)
